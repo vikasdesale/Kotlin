44428ce4c8894e42935a745e9e96e7ac421598b6
vikasdesale <vikas.desaleu@gmail.com>
2017-06-30 23:52:29 +0530

    Functions

---
+++ 11.Functions/src/VariableScope.kt
@@ -0,0 +1,15 @@
+/**
+ * Created by Dell on 6/30/2017.
+ */
+
+var b:Int=20   //global variable
+//User defined function
+fun add(b:Int) : Unit{
+    println(b)
+}
+
+fun main(args:Array<String>){
+    var a=10
+    var total=a+b
+    println(total)
+}
---
+++ 11.Functions/src/functionAdd.kt
@@ -0,0 +1,13 @@
+/**
+ * Created by Dell on 6/30/2017.
+ */
+//User defined function
+fun add(a:Double, b:Double) : Double{
+  var add =a+b
+return add
+}
+
+fun main(args:Array<String>){
+var total=add(10.0,20.0);
+println(total)
+}
