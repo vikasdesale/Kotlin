8bc614b24c2d90140d10b75e89d5f312d0bf8614
vikasdesale <vikas.desaleu@gmail.com>
2017-06-30 23:22:11 +0530

    while_and_do_while_loop

--- 9.Loops/src/forLoop.kt
+++ 9.Loops/src/forLoop.kt
@@ -6,6 +6,6 @@ fun main(args:Array<String>){
     //For Loop
     for(number in 0..10)
     {
-        print(number)
+        println(number)
     }
 }
---
+++ 9.Loops/src/whileLoop.kt
@@ -0,0 +1,11 @@
+/**
+ * Created by Dell on 6/30/2017.
+ */
+fun main(args:Array<String>){
+    var counter = 1
+   while(counter<=10)
+    {
+        println(counter)
+        counter++
+    }
+}
