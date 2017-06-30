75b1aa63e62b98cab9ace969b77e5d0c8f3fdc77
vikasdesale <vikas.desaleu@gmail.com>
2017-06-30 23:12:12 +0530

    WhenStatement

---
+++ 8.ConditionalStatements/src/When.kt
@@ -0,0 +1,15 @@
+/**
+ * Created by Dell on 6/30/2017.
+ */
+fun main(args:Array<String>){
+
+    //When is like switch statement
+    var a:Int=50
+
+    when(a)
+    {
+        50 -> { println(" A's values is 50") }   //when value of A is 50 print this option
+        60 -> { println(" A's values is 50") } //when value of A is 60 print this option
+    }
+
+}
