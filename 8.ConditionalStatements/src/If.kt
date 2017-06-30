410b05cad6ee9b0bce19a9c1b7df5104182636bc
vikasdesale <vikas.desaleu@gmail.com>
2017-06-30 22:59:45 +0530

    IfStatement

---
+++ 8.ConditionalStatements/src/If.kt
@@ -0,0 +1,15 @@
+/**
+ * Created by Dell on 6/30/2017.
+ */
+fun main(args:Array<String>){
+
+   //If Statement
+    var a:Int=50
+    var b:Int=20
+
+    if(a>b)
+    {
+        println(" A is Greater than b")
+    }
+
+}
